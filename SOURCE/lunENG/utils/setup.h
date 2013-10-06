26074b43942aea8c343ab79db3a6be1e77b39607
Kirill Kranz <thegreatkirill@freenet.de>
2013-10-06 06:01:43 +0200

    starting
    the start 2013.10.06 06:01

---
+++ SOURCE/lunENG/utils/setup.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+
+using namespace std;
+
+extern int rendererver;
+extern int screenwidth;
+extern int screenheight;
+extern bool screenFullScreen;
+extern string DATAfolder;
+
+extern bool screenFullScreen;
+extern int how2showFPS;
+extern bool showMouse;
+
+
+void GetEngineStartupConfig();
