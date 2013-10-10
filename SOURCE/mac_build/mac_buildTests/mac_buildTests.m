ada852e0ac028a126c9afc764424725fd2809dd2
Kirill Kranz <thegreatkirill@freenet.de>
2013-10-10 09:04:48 +0200

    xcore5 update

---
+++ SOURCE/mac_build/mac_buildTests/mac_buildTests.m
@@ -0,0 +1,34 @@
+//
+//  mac_buildTests.m
+//  mac_buildTests
+//
+//  Created by Kirill Kranz on 10.10.13.
+//  Copyright (c) 2013 Project Luna Org. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface mac_buildTests : XCTestCase
+
+@end
+
+@implementation mac_buildTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
